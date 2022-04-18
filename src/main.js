56cdafb551154b74dd1be38568d62de7c239e521
amine aqebli <aqebli36@gmail.com>
2022-04-18 05:44:00 +0000

    badge/buttton/card components show data inside

--- src/main.js
+++ src/main.js
@@ -3,4 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App);
+
+
+
+//Global Components
+import BaseCard from './components/layout/ui/BaseCard.vue';
+import BaseButton from './components/layout/ui/BaseButton.vue';
+import BaseBadge from './components/layout/ui/BaseBadge.vue';
+app.component('base-button', BaseButton);
+app.component('base-card', BaseCard);
+app.component('base-badge', BaseBadge);
+
+
+app.use(store);
+app.use(router);
+app.mount('#app');
--- src/views/coaches/CoachesList.vue
+++ src/views/coaches/CoachesList.vue
@@ -1,28 +1,32 @@
 <template>
     <section>Filter</section>
     <section>
-        <div class="controls">
-            <button>Refresh</button> 
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <coach-item 
-            v-for="coach in filtredCoached" 
-            :key="coach.id" 
-            :id="coach.id" 
-            :first-name="coach.firstName" 
-            :last-name="coach.lastName" 
-            :rate = "coach.hourlyRate"
-            :areas="coach.areas"
-            ></coach-item>
-        </ul>
-        <h3 v-else>No Coaches Found.</h3>
-
+        <base-card>
+            <div class="controls">
+                <base-button mode="outline">Refresh</base-button> 
+                <base-button to="/register">Register as Coach</base-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <coach-item 
+                v-for="coach in filtredCoached" 
+                :key="coach.id" 
+                :id="coach.id" 
+                :first-name="coach.firstName" 
+                :last-name="coach.lastName" 
+                :rate = "coach.hourlyRate"
+                :areas="coach.areas"
+                ></coach-item>
+            </ul>
+            <h3 v-else>No Coaches Found.</h3>
+    </base-card>
     </section>
 </template>
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue';
+
+
+
 
 export default {
 components: { CoachItem },
