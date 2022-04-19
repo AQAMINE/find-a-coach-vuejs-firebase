7220f59828df10b2c15bfad3d77b95448c600c8b
amine aqebli <aqebli36@gmail.com>
2022-04-19 01:53:48 +0000

    coach detail

--- src/components/layout/ui/BaseBadge.vue
+++ src/components/layout/ui/BaseBadge.vue
@@ -1,6 +1,6 @@
 <template>
     <span class="badge" :class="type">
-        {{text}}
+        {{ text }}
     </span>
 </template>
 
--- src/components/layout/ui/BaseCard.vue
+++ src/components/layout/ui/BaseCard.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="card">
-        <slot>
-            
-        </slot>
+        <slot></slot>
     </div>
 
 </template>
